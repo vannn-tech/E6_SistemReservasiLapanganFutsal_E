6244a213bb22521d9f9ddbf6167d72a9fe224c63
vannn-tech <jovan20166@gmail.com>
2026-04-15 01:51:05 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Reservasi_Futsal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvLapangan = new System.Windows.Forms.DataGridView();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtLokasi = new System.Windows.Forms.TextBox();
+            this.txtCari = new System.Windows.Forms.TextBox();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLapangan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLapangan
+            // 
+            this.dgvLapangan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLapangan.Location = new System.Drawing.Point(377, 127);
+            this.dgvLapangan.Name = "dgvLapangan";
+            this.dgvLapangan.Size = new System.Drawing.Size(392, 262);
+            this.dgvLapangan.TabIndex = 0;
+            this.dgvLapangan.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLapangan_CellClick);
+            this.dgvLapangan.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(130, 127);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(130, 165);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(167, 20);
+            this.txtNama.TabIndex = 2;
+            // 
+            // txtLokasi
+            // 
+            this.txtLokasi.Location = new System.Drawing.Point(130, 206);
+            this.txtLokasi.Name = "txtLokasi";
+            this.txtLokasi.Size = new System.Drawing.Size(167, 20);
+            this.txtLokasi.TabIndex = 3;
+            // 
+            // txtCari
+            // 
+            this.txtCari.Location = new System.Drawing.Point(414, 87);
+            this.txtCari.Name = "txtCari";
+            this.txtCari.Size = new System.Drawing.Size(228, 20);
+            this.txtCari.TabIndex = 4;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "Tersedia",
+            "Tidak Tersedia"});
+            this.cmbStatus.Location = new System.Drawing.Point(130, 250);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(121, 21);
+            this.cmbStatus.TabIndex = 5;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(28, 330);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 6;
+            this.btnInsert.Text = "Simpan";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(142, 330);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Ubah";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(28, 388);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Hapus";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Location = new System.Drawing.Point(142, 388);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(75, 23);
+            this.btnTampilkan.TabIndex = 9;
+            this.btnTampilkan.Text = "View";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            this.btnTampilkan.Click += new System.EventHandler(this.btnTampilkan_Click);
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(663, 87);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(75, 23);
+            this.btnCari.TabIndex = 10;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Manajemen Data Lapangan Futsal";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "NAMA LAPANGAN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "ALAMAT/LOKASI";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "STATUS";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(641, 25);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(84, 13);
+            this.lblTotal.TabIndex = 16;
+            this.lblTotal.Text = "Total Record : 0";
+            this.lblTotal.Click += new System.EventHandler(this.lblTotal_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Connect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.txtCari);
+            this.Controls.Add(this.txtLokasi);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.dgvLapangan);
+            this.Name = "Form1";
+            this.Text = "Reservasi";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLapangan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvLapangan;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtLokasi;
+        private System.Windows.Forms.TextBox txtCari;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnTampilkan;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Bagian A: Pengenalan ADO.NET (Namespace yang diperlukan)
+using System.Data.SqlClient;
+
+namespace Reservasi_Futsal
+{
+    public partial class Form1 : Form
+    {
+        // Connection String - Menggunakan Server LAPTOP-5R80O1Q5\MSSQLSERVER01
+        string connectionString = @"Data Source=LAPTOP-5R80O1Q5\MSSQLSERVER01;Initial Catalog=DBFutsalADO;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set txtID menjadi ReadOnly karena ID otomatis dari database
+            txtID.ReadOnly = true;
+
+            // Bagian B: Koneksi Database saat aplikasi pertama kali dijalankan
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    // Jika berhasil terbuka, langsung muat data ke GridView
+                }
+                catch (Exception ex)
+                {
+                    // Memberikan informasi jika koneksi gagal saat startup
+                    MessageBox.Show("Koneksi Database Gagal saat Startup: " + ex.Message, "Error Koneksi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // ==========================================
+        // FITUR: BUTTON CONNECT (Validasi Koneksi Manual)
+        // ==========================================
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    MessageBox.Show("Koneksi Database Berhasil!", "Status Koneksi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Koneksi Gagal: " + ex.Message, "Error Koneksi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Bagian D: Implementasikan ExecuteScalar untuk menghitung total record
+        private void HitungTotalRecord()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string query = "SELECT COUNT(*) FROM Lapangan";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    conn.Open();
+                    int total = (int)cmd.ExecuteScalar(); // Bagian D: ExecuteScalar
+                    lblTotal.Text = "Total Record: " + total.ToString();
+                }
+                catch { /* Abaikan jika terjadi error pada perhitungan */ }
+            }
+        }
+
+        // Bagian E: Tombol Tampilkan Data menggunakan SqlDataReader
+        private void btnTampilkan_Click(object sender, EventArgs e)
+        {
+            TampilkanData();
+            MessageBox.Show("Berhasil Menampilkan Data.", "Informasi");
+        }
+
+        // Fungsi utama untuk memuat data ke DataGridView
+        private void TampilkanData()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("LapanganID");
+            dt.Columns.Add("NamaLapangan");
+            dt.Columns.Add("Lokasi");
+            dt.Columns.Add("Status");
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Lapangan", conn);
+                    conn.Open();
+                    // Bagian E: Menampilkan data menggunakan SqlDataReader
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        dt.Rows.Add(reader["LapanganID"], reader["NamaLapangan"], reader["Lokasi"], reader["Status"]);
+                    }
+                    dgvLapangan.DataSource = dt;
+
+                    // Penyesuaian agar baris jadi biru saat diklik & tidak bisa diketik
+                    dgvLapangan.SelectionMode = DataGridViewSelectionMode.FullRowSelect; // Full Row Biru
+                    dgvLapangan.ReadOnly = true; // Tidak bisa diketik/edit di Grid
+                    dgvLapangan.AllowUserToAddRows = false; // Menghilangkan baris kosong bawah
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal mengambil data: " + ex.Message);
+                }
+            }
+            HitungTotalRecord();
+        }
+
+        // Bagian D: Implementasikan query INSERT menggunakan SqlCommand dan ExecuteNonQuery
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            // Bagian F: Validasi input (Field penting tidak boleh kosong)
+            if (string.IsNullOrEmpty(txtNama.Text) || string.IsNullOrEmpty(cmbStatus.Text))
+            {
+                MessageBox.Show("Nama dan Status wajib diisi!", "Peringatan");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Lapangan (NamaLapangan, Lokasi, Status) VALUES (@nama, @lokasi, @status)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@nama", txtNama.Text);
+                cmd.Parameters.AddWithValue("@lokasi", txtLokasi.Text);
+                cmd.Parameters.AddWithValue("@status", cmbStatus.Text);
+
+                conn.Open();
+                cmd.ExecuteNonQuery(); // Bagian D: ExecuteNonQuery
+                MessageBox.Show("Data Lapangan Berhasil Ditambahkan!");
+                TampilkanData();
+            }
+        }
+
+        // Bagian D: Implementasikan query UPDATE menggunakan SqlCommand dan ExecuteNonQuery
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Bagian F: Validasi input (Pastikan data sudah dipilih)
+            if (string.IsNullOrEmpty(txtID.Text))
+            {
+                MessageBox.Show("Pilih data di tabel terlebih dahulu!", "Peringatan");
+                return;
+            }
+
+            // Bagian F: Tambahkan konfirmasi sebelum data diubah
+            DialogResult dr = MessageBox.Show("Yakin ingin mengubah data?", "Konfirmasi Update", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Lapangan SET NamaLapangan=@nama, Lokasi=@lokasi, Status=@status WHERE LapanganID=@id";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", txtID.Text);
+                    cmd.Parameters.AddWithValue("@nama", txtNama.Text);
+                    cmd.Parameters.AddWithValue("@lokasi", txtLokasi.Text);
+                    cmd.Parameters.AddWithValue("@status", cmbStatus.Text);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery(); // Bagian D: ExecuteNonQuery
+                    MessageBox.Show("Data Berhasil Diperbarui!");
+                    TampilkanData();
+                }
+            }
+        }
+
+        // Bagian D: Implementasikan query DELETE menggunakan SqlCommand dan ExecuteNonQuery
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtID.Text))
+            {
+                MessageBox.Show("Pilih data yang akan dihapus!");
+                return;
+            }
+
+            // Bagian F: Tambahkan konfirmasi sebelum data dihapus
+            DialogResult dr = MessageBox.Show("Hapus data ini?", "Konfirmasi Hapus", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dr == DialogResult.Yes)
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM Lapangan WHERE LapanganID=@id";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", txtID.Text);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery(); // Bagian D: ExecuteNonQuery
+                    MessageBox.Show("Data Terhapus!");
+                    TampilkanData();
+                }
+            }
+        }
+
+        // Bagian E: Fitur pencarian data (Teks, Angka, Kapital/Kecil tetap Valid)
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Query menggunakan LIKE untuk pencarian fleksibel
+                    // SQL Server secara default tidak membedakan Kapital/Kecil (CI - Case Insensitive)
+                    string query = "SELECT * FROM Lapangan WHERE NamaLapangan LIKE @cari OR Lokasi LIKE @cari OR Status LIKE @cari";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    // Mengambil input dan menghapus spasi di awal/akhir agar pencarian lebih akurat
+                    string kataKunci = txtCari.Text.Trim();
+                    cmd.Parameters.AddWithValue("@cari", "%" + kataKunci + "%");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    // Tampilkan hasil ke GridView
+                    dgvLapangan.DataSource = dt;
+
+                    // Jika hasil tidak ditemukan, berikan informasi
+                    if (dt.Rows.Count == 0)
+                    {
+                        MessageBox.Show("Data tidak ditemukan.", "Pencarian");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Terjadi kesalahan saat mencari: " + ex.Message);
+                }
+            }
+        }
+
+        // Bagian E: Fitur pilih data dari DataGridView ke TextBox
+        private void dgvLapangan_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvLapangan.Rows[e.RowIndex];
+                txtID.Text = row.Cells["LapanganID"].Value.ToString();
+                txtNama.Text = row.Cells["NamaLapangan"].Value.ToString();
+                txtLokasi.Text = row.Cells["Lokasi"].Value.ToString();
+                cmbStatus.Text = row.Cells["Status"].Value.ToString();
+            }
+        }
+
+        // Event handler lainnya dibiarkan kosong
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label4_Click(object sender, EventArgs e) { }
+        private void label5_Click(object sender, EventArgs e) { }
+        private void label2_Click(object sender, EventArgs e) { }
+        private void lblTotal_Click(object sender, EventArgs e) { }
+    }
+}
